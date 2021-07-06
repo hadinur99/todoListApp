d90004d0dbe76f55bd0918f7c3eebd82e656f845
Hadi N <hadinurdin99@gamil.com>
2021-07-06 15:31:32 +0700

    commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,23 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'May 5th',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Buy Groceries',
+        day: 'May 5th',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Dentist Appointment',
+        day: 'May 5th',
+        reminder: false
+    }
+
+]
